55401ada41175b8e01e27d39eefba069d3b55a7f
joshahuynh <joshahuynh@gmail.com>
2022-03-19 22:39:21 -0500

    almost working map

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,57 +6,48 @@ $('.datepicker').datepicker({
 
 })
 
-var lat;
-var lon;
 var map;
 var mapOptions;
 var cityName;
-var submitBtn = $('submit-button')
-var destinationVal = $('#destination-value')
+var submitBtn = $('#submit-button')
 var cardDisplay = $('.card')
+var destinationVal = $('#destination-value')
 
 submitBtn.on("click", function(event){
     event.preventDefault();
     cityName = $(destinationVal).val();
-    destinationVal.val('')
-    cardDisplay.empty();
+    console.log(destinationVal);
     initMap();
 })
 
 function initMap(){
-    let location=new Object();
     var apiKey="b1230e9ae6629f281894dc4555b0c16d";
-    cityName= ""
-    var apiUrlGeoCood=`https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${apiKey}`;
+    var apiUrlGeoCood=`https://api.openweathermap.org/geo/1.0/direct?q=${destinationVal}&appid=${apiKey}`;
     fetch(apiUrlGeoCood)
-        .then(function(response){
-            if (response.ok){
-                response.json().then(function(data){
-                    console.log(data)
-                        lat=data[0].lat;
-                        lon=data[0].lon;
-                       
-                    location.lat=lat
-                    location.long=lon 
-                    console.log(location.lon)
-                        
-                    mapOptions =new google.maps.Map(document.getElementById('map'),{
-                        center: {lat:lat, lng:lon},
-                        zoom:15
-                    })
-                })
-            
-                
+        .then(function(response) {
+            if (response.ok) {
+                response.json()
+                    .then(function(data) {
+                        if (data.length > 0) {
+                            console.log(data)
+                            var lat=data[0].lat;
+                            var lon=data[0].lon;
+                            
+                            map = new google.maps.Map(document.getElementById('map'), {
+                                center: {lat: lat, lng: lon},
+                                zoom:15
+                            })
+                        }
+                    })  
             }    
         })
-    }
+}
+// map = new google.maps.Map(document.getElementById("map"), mapOptions);
+// initMap();
+// var marker = new google.maps.Marker({
+//     location: {lat:lat, lng:lon},
+//     radius:'5000',
+//     type: "tourist_attraction",
+// });
 
-var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-
-var marker = new google.maps.Marker({
-    location: {lat:lat, lng:lon},
-    radius:'5000',
-    type: "tourist_attraction",
-});
-
-marker.setMap(map);
+// marker.setMap(map);
